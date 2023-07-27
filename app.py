450adda00bd86625976bad362fdc48df89690e81
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-27 14:24:44 +0300

    base.html

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+
+
 import sqlite3
 from flask import Flask, render_template, redirect, url_for, flash, request
 
@@ -38,3 +40,4 @@ def create():
             return redirect(url_for('index'))
 
     return render_template('create.html')
+
