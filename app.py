dddbdf2fb1585db21c4b7ad1ee59b06fd1c20d1e
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-27 14:36:39 +0300

    edit post

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-
-
 import sqlite3
-from flask import Flask, render_template, redirect, url_for, flash, request
+from flask import Flask, render_template, redirect, url_for, flash, request, abort
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '76bc3254cacc9d8d5d8e5461451b460e70dc4c8db9ee3d2d'
@@ -11,6 +9,15 @@ def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM WHERE id = ?', (post_id,)).fetchone()
+
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
 
 
 @app.route('/')
